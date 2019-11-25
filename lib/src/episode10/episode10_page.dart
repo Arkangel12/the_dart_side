f5db636930061b9a35d6e8a5a67ab120f2c6c499
Argel Bejarano <argel.bc18@gmail.com>
2019-11-25 14:38:54 -0700

    some changes

---
+++ lib/src/episode10/episode10_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Episode10Page extends StatefulWidget {
+  @override
+  _Episode10PageState createState() => _Episode10PageState();
+}
+
+class _Episode10PageState extends State<Episode10Page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
