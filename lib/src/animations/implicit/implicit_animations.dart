c95855a3152cb1d6081bb67f0d3db3f17ec5b9ab
Diego Velásquez <diego.velasquez.lopez@gmail.com>
2020-01-09 16:28:30 -0500

    animations

--- lib/src/animations/implicit/implicit_animations.dart
+++ lib/src/animations/implicit/implicit_animations.dart
@@ -6,13 +6,47 @@ class ImplicitAnimations extends StatefulWidget {
 }
 
 class _ImplicitAnimationsState extends State<ImplicitAnimations> {
+  double squareSize = 80;
+  bool circular = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          "Implicits Animations",
+          "Implicit Animations",
         ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.play_arrow),
+            onPressed: () {
+              setState(() {
+                circular = !circular;
+              });
+            },
+          )
+        ],
+      ),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset(
+            "assets/images/background.jpeg",
+            fit: BoxFit.cover,
+          ),
+          Align(
+            alignment: circular ? Alignment.topLeft : Alignment.bottomRight,
+            child: Container(
+              decoration: BoxDecoration(
+                color: circular ? Colors.red : Colors.blue,
+                borderRadius:
+                    circular ? BorderRadius.circular(squareSize / 2) : null,
+              ),
+              height: squareSize,
+              width: squareSize,
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/src/animations/transition/transition_animations.dart
+++ lib/src/animations/transition/transition_animations.dart
@@ -6,13 +6,58 @@ class TransitionAnimations extends StatefulWidget {
 }
 
 class _TransitionAnimationsState extends State<TransitionAnimations> {
+  double squareSize = 80;
+  bool circular = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          "Implicits Animations",
+          "Transition Animations",
         ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.play_arrow),
+            onPressed: () {
+              setState(() {
+                circular = !circular;
+              });
+            },
+          )
+        ],
+      ),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset(
+            "assets/images/background.jpeg",
+            fit: BoxFit.cover,
+          ),
+          TweenAnimationBuilder<double>(
+            tween: circular
+                ? Tween(begin: 0.0, end: 1.0)
+                : Tween(begin: 10, end: 0.0),
+            duration: const Duration(seconds: 3),
+            builder: (context, value, widget) {
+              final alignment = Alignment.lerp(
+                  Alignment.topLeft, Alignment.bottomRight, value);
+              return Align(
+                alignment: alignment,
+                child: AnimatedContainer(
+                  duration: const Duration(seconds: 2),
+                  decoration: BoxDecoration(
+                    color: circular ? Colors.red : Colors.blue,
+                    borderRadius:
+                        circular ? BorderRadius.circular(squareSize / 2) : null,
+                  ),
+                  height: squareSize,
+                  width: squareSize,
+                ),
+              );
+            },
+          ),
+        ],
       ),
     );
   }
