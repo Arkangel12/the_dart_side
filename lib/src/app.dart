5abeadcab932342ab7dcb475c617ead65b8b8cbe
Argel Bejarano <argel.bc18@gmal.com>
2020-05-01 17:33:27 -0700

    changes

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:the_dart_side/src/hackerclub/hackerClub.dart';
 
 //import 'package:the_dart_side/src/animations2/implicitAnimations.dart';
 //import 'package:the_dart_side/src/episode7/episode7_page.dart';
@@ -24,6 +25,18 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class Cosa extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cosa'),
+      ),
+      body: Container(),
+    );
+  }
+}
+
 class MenuPage extends StatelessWidget {
   _openNewPage(BuildContext context, Widget page) {
     Navigator.of(context).push(
@@ -43,25 +56,32 @@ class MenuPage extends StatelessWidget {
               Image.asset('assets/images/dash_vader_white.png', height: 200),
               SizedBox(height: 20),
               RaisedButton(
-                child: Text("Appbar Personalizada"),
-                onPressed: () => _openNewPage(context, Episode3Page()),
+                child: Text("Hola HackerClub"),
+                onPressed: () => _openNewPage(
+                  context,
+                  HackerClub(),
+                ),
               ),
-              RaisedButton(
-                child: Text("Responsive"),
-                onPressed: () => _openNewPage(context, Episode7Page()),
-              ),
-              RaisedButton(
-                child: Text("Episode VIII: Multilayout"),
-                onPressed: () => _openNewPage(context, Episode8Page()),
-              ),
-              RaisedButton(
-                child: Text("Animaciones Implicitas"),
-//                onPressed: () => _openNewPage(context, ImplicitAnimations()),
-              ),
-              RaisedButton(
-                child: Text("Animaciones Implicitas"),
-//                onPressed: () => _openNewPage(context, ImplicitAnimations()),
-              ),
+//              RaisedButton(
+//                child: Text("Appbar Personalizada"),
+//                onPressed: () => _openNewPage(context, Episode3Page()),
+//              ),
+//              RaisedButton(
+//                child: Text("Responsive"),
+//                onPressed: () => _openNewPage(context, Episode7Page()),
+//              ),
+//              RaisedButton(
+//                child: Text("Episode VIII: Multilayout"),
+//                onPressed: () => _openNewPage(context, Episode8Page()),
+//              ),s
+//              RaisedButton(
+//                child: Text("Animaciones Implicitas"),
+////                onPressed: () => _openNewPage(context, ImplicitAnimations()),
+//              ),
+//              RaisedButton(
+//                child: Text("Animaciones Implicitas"),
+////                onPressed: () => _openNewPage(context, ImplicitAnimations()),
+//              ),
               RaisedButton(
                 child: Text("Supernova"),
                 onPressed: () => _openNewPage(
@@ -69,20 +89,20 @@ class MenuPage extends StatelessWidget {
                   SupernovaTest(),
                 ),
               ),
-              RaisedButton(
-                child: Text("Multilayout"),
-                onPressed: () => _openNewPage(
-                  context,
-                  Episode8Page(),
-                ),
-              ),
-              RaisedButton(
-                child: Text("Animations"),
-                onPressed: () => _openNewPage(
-                  context,
-                  MainAnimations(),
-                ),
-              ),
+//              RaisedButton(
+//                child: Text("Multilayout"),
+//                onPressed: () => _openNewPage(
+//                  context,
+//                  Episode8Page(),
+//                ),
+//              ),
+//              RaisedButton(
+//                child: Text("Animations"),
+//                onPressed: () => _openNewPage(
+//                  context,
+//                  MainAnimations(),
+//                ),
+//              ),
             ],
           ),
         ),
